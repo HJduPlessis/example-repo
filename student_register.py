f79cad282945066352a031a224ae7952b197e390
HJduPlessis <hjduplessis3004@gmail.com>
2026-06-24 13:19:45 +0200

    Delete student_register.py

--- student_register.py
+++
@@ -1,6 +0,0 @@
-with open("reg_form.txt",'a+') as file:
-    student_count=int(input("How many syudents are registering: "));
-    student_id=[];
-    for i in range(student_count):
-        student_id.append(input("Enter student id: "));
-        file.write(student_id[i]+"..........."+"\n");
